ece8ba20bd3ae6af328e2de51aed0c39dca8f48b
Octavio Roncal <octavioroncal@monentia.es>
2016-11-15 22:10:06 +0000

    added paste processor to import maven dependencies

--- legio/src/io/intino/legio/plugin/file/LegioDependencyPasteProcessor.java
+++ legio/src/io/intino/legio/plugin/file/LegioDependencyPasteProcessor.java
@@ -1,33 +1,43 @@
 package io.intino.legio.plugin.file;
 
-import com.intellij.codeInsight.editorActions.CopyPastePostProcessor;
+import com.intellij.codeInsight.editorActions.CopyPastePreProcessor;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.RangeMarker;
+import com.intellij.openapi.editor.RawText;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Ref;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.awt.datatransfer.Transferable;
-import java.util.Collections;
-import java.util.List;
+public class LegioDependencyPasteProcessor implements CopyPastePreProcessor {
 
-public class LegioDependencyPasteProcessor extends CopyPastePostProcessor {
-	@NotNull
+	private static final String GROUP_ID = "<groupId>";
+	private static final String ARTIFACT_ID = "<artifactId>";
+	private static final String VERSION = "<version>";
+
+	@Nullable
 	@Override
-	public List collectTransferableData(PsiFile psiFile, Editor editor, int[] ints, int[] ints1) {
-		return Collections.emptyList();
-
+	public String preprocessOnCopy(PsiFile psiFile, int[] ints, int[] ints1, String s) {
+		return s;
 	}
 
 	@NotNull
 	@Override
-	public List extractTransferableData(Transferable content) {
-		return super.extractTransferableData(content);
+	public String preprocessOnPaste(Project project, PsiFile psiFile, Editor editor, String text, RawText rawText) {
+		if (!psiFile.getFileType().equals(LegioFileType.instance()) || !isMavenDependency(text.trim())) return text;
+		String[] parameters = extractInfoFrom(text);
+		return parameters[0] + "(\"" + parameters[1] + "\", \"" + parameters[2] + "\", \"" + parameters[3] + "\")";
 	}
 
-	@Override
-	public void processTransferableData(Project project, Editor editor, RangeMarker bounds, int caretOffset, Ref indented, List values) {
-		super.processTransferableData(project, editor, bounds, caretOffset, indented, values);
+	private boolean isMavenDependency(String text) {
+		return text.startsWith("<dependency>") && text.endsWith("</dependency>") &&
+				text.contains("<groupId>") && text.contains("<artifactId>");
+	}
+
+	private String[] extractInfoFrom(String text) {
+		String scope = text.contains("<scope>test</scope>") ? "Test" : "Compile";
+		String groupId = text.substring(text.indexOf(GROUP_ID) + GROUP_ID.length(), text.indexOf("</groupId>"));
+		String artifactId = text.substring(text.indexOf(ARTIFACT_ID) + ARTIFACT_ID.length(), text.indexOf("</artifactId>"));
+		String version = text.substring(text.indexOf(VERSION) + VERSION.length(), text.indexOf("</version>"));
+		return new String[]{scope, groupId, artifactId, version};
 	}
 }
