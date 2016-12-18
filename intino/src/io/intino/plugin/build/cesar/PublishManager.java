e2b39d4bc7a59c77c1929b2ca3daffc3f602a9ec
Octavio Roncal <octavioroncal@monentia.es>
2016-12-18 17:04:54 +0000

    improving publishing

--- intino/src/io/intino/plugin/build/cesar/PublishManager.java
+++ intino/src/io/intino/plugin/build/cesar/PublishManager.java
@@ -1,12 +1,21 @@
 package io.intino.plugin.build.cesar;
 
 import com.intellij.openapi.module.Module;
+import io.intino.cesar.RestCesarAccessor;
+import io.intino.cesar.schemas.Artifactory;
+import io.intino.cesar.schemas.SystemSchema;
 import io.intino.legio.LifeCycle;
+import io.intino.pandora.exceptions.Unknown;
 import io.intino.plugin.build.LifeCyclePhase;
 import io.intino.plugin.project.LegioConfiguration;
 import tara.intellij.lang.psi.impl.TaraUtil;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PublishManager {
 	private final LifeCyclePhase phase;
@@ -21,10 +30,39 @@ public class PublishManager {
 
 	public void execute() {
 		final LifeCycle.Publishing publishing = configuration.lifeCycle().publishing();
-		final List<? extends LifeCycle.Publishing.Destiny> destinies = phase.equals(LifeCyclePhase.PREDEPLOY) ? publishing.preDeployList() : publishing.deployList();
+		final List<? extends LifeCycle.Publishing.Destination> destinies = phase.equals(LifeCyclePhase.PREDEPLOY) ? publishing.preDeployList() : publishing.deployList();
 		for (LifeCycle.Publishing.Destiny destiny : destinies) {
-//			new CessarAccesor(publishing.cesarURL()).publish(destiny.publicURL(),)
+			try {
+				new RestCesarAccessor(urlOf(publishing)).postDeploySystem(createSystem());
+			} catch (Unknown unknown) {
+				unknown.printStackTrace();
+			}
 		}
 
+	}
+
+	private SystemSchema createSystem() {
+		return new SystemSchema().artifact(configuration.artifactId()).artifactoryList(artifactories()).parameters(extractParameters());
+	}
+
+	private List<String> extractParameters() {
+
+
+	}
+
+	private List<Artifactory> artifactories() {
+		List<Artifactory> artifactories = new ArrayList<>();
+		AbstractMap.SimpleEntry<String, String> distribution = configuration.distributionReleaseRepository();
+		artifactories.add(new Artifactory().url(distribution.getKey()).id(distribution.getValue()));
+		artifactories.addAll(configuration.releaseRepositories().entrySet().stream().map(entry -> new Artifactory().url(entry.getKey()).id(entry.getValue())).collect(Collectors.toList()));
+		return artifactories;
+	}
+
+	private URL urlOf(LifeCycle.Publishing publishing) {
+		try {
+			return new URL(publishing.cesarURL());
+		} catch (MalformedURLException e) {
+			return null;
+		}
 	}
 }
