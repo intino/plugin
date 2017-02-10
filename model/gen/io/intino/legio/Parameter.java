9812cf272fb817faa7d8bf5913724f3ad0731a17
Octavio Roncal <octavioroncal@monentia.es>
2017-02-10 13:30:48 +0000

    updated model. Exposed RunConfigurations in legio api

--- model/gen/io/intino/legio/Parameter.java
+++ model/gen/io/intino/legio/Parameter.java
@@ -4,114 +4,24 @@ import io.intino.legio.*;
 
 
 public class Parameter extends io.intino.tara.magritte.Layer implements io.intino.tara.magritte.tags.Component, io.intino.tara.magritte.tags.Terminal {
-	protected boolean required;
+	protected java.lang.String value;
 
 	public Parameter(io.intino.tara.magritte.Node node) {
 		super(node);
 	}
 
-	public boolean required() {
-		return required;
+	public java.lang.String value() {
+		return value;
 	}
 
-	public void required(boolean value) {
-		this.required = value;
-	}
-
-	public io.intino.legio.type.TypeParameter asType() {
-		return this.as(io.intino.legio.type.TypeParameter.class);
-	}
-
-	public io.intino.legio.type.TypeParameter asType(io.intino.tara.magritte.Expression<java.lang.String> type) {
-		io.intino.legio.type.TypeParameter newElement = addFacet(io.intino.legio.type.TypeParameter.class);
-		newElement.node().set(newElement, "type", java.util.Collections.singletonList(type)); 
-	    return newElement;
-	}
-
-	public boolean isType() {
-		return is(io.intino.legio.type.TypeParameter.class);
-	}
-
-	public void removeType() {
-		this.removeFacet(io.intino.legio.type.TypeParameter.class);
-	}
-
-	public io.intino.legio.number.NumberParameter asNumber() {
-		return this.as(io.intino.legio.number.NumberParameter.class);
-	}
-
-	public io.intino.legio.number.NumberParameter asNumber(int value) {
-		io.intino.legio.number.NumberParameter newElement = addFacet(io.intino.legio.number.NumberParameter.class);
-		newElement.node().set(newElement, "value", java.util.Collections.singletonList(value)); 
-	    return newElement;
-	}
-
-	public boolean isNumber() {
-		return is(io.intino.legio.number.NumberParameter.class);
-	}
-
-	public void removeNumber() {
-		this.removeFacet(io.intino.legio.number.NumberParameter.class);
-	}
-
-	public io.intino.legio.bool.BoolParameter asBool() {
-		return this.as(io.intino.legio.bool.BoolParameter.class);
-	}
-
-	public io.intino.legio.bool.BoolParameter asBool(boolean value) {
-		io.intino.legio.bool.BoolParameter newElement = addFacet(io.intino.legio.bool.BoolParameter.class);
-		newElement.node().set(newElement, "value", java.util.Collections.singletonList(value)); 
-	    return newElement;
-	}
-
-	public boolean isBool() {
-		return is(io.intino.legio.bool.BoolParameter.class);
-	}
-
-	public void removeBool() {
-		this.removeFacet(io.intino.legio.bool.BoolParameter.class);
-	}
-
-	public io.intino.legio.real.RealParameter asReal() {
-		return this.as(io.intino.legio.real.RealParameter.class);
-	}
-
-	public io.intino.legio.real.RealParameter asReal(double value) {
-		io.intino.legio.real.RealParameter newElement = addFacet(io.intino.legio.real.RealParameter.class);
-		newElement.node().set(newElement, "value", java.util.Collections.singletonList(value)); 
-	    return newElement;
-	}
-
-	public boolean isReal() {
-		return is(io.intino.legio.real.RealParameter.class);
-	}
-
-	public void removeReal() {
-		this.removeFacet(io.intino.legio.real.RealParameter.class);
-	}
-
-	public io.intino.legio.text.TextParameter asText() {
-		return this.as(io.intino.legio.text.TextParameter.class);
-	}
-
-	public io.intino.legio.text.TextParameter asText(java.lang.String value) {
-		io.intino.legio.text.TextParameter newElement = addFacet(io.intino.legio.text.TextParameter.class);
-		newElement.node().set(newElement, "value", java.util.Collections.singletonList(value)); 
-	    return newElement;
-	}
-
-	public boolean isText() {
-		return is(io.intino.legio.text.TextParameter.class);
-	}
-
-	public void removeText() {
-		this.removeFacet(io.intino.legio.text.TextParameter.class);
+	public void value(java.lang.String value) {
+		this.value = value;
 	}
 
 	@Override
 	public java.util.Map<java.lang.String, java.util.List<?>> variables() {
 		java.util.Map<String, java.util.List<?>> map = new java.util.LinkedHashMap<>();
-		map.put("required", new java.util.ArrayList(java.util.Collections.singletonList(this.required)));
+		map.put("value", new java.util.ArrayList(java.util.Collections.singletonList(this.value)));
 		return map;
 	}
 
@@ -122,13 +32,13 @@ public class Parameter extends io.intino.tara.magritte.Layer implements io.intin
 	@Override
 	protected void _load(java.lang.String name, java.util.List<?> values) {
 		super._load(name, values);
-		if (name.equalsIgnoreCase("required")) this.required = io.intino.tara.magritte.loaders.BooleanLoader.load(values, this).get(0);
+		if (name.equalsIgnoreCase("value")) this.value = io.intino.tara.magritte.loaders.StringLoader.load(values, this).get(0);
 	}
 
 	@Override
 	protected void _set(java.lang.String name, java.util.List<?> values) {
 		super._set(name, values);
-		if (name.equalsIgnoreCase("required")) this.required = (java.lang.Boolean) values.get(0);
+		if (name.equalsIgnoreCase("value")) this.value = (java.lang.String) values.get(0);
 	}
 
 	public Create create() {
