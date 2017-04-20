f44b4bebe98ab7ea717eb4d8c29a8fb6a2d62e4e
Octavio Roncal <octavioroncal@monentia.es>
2017-04-20 10:55:42 +0100

    fixed minor issues

--- intino/src/io/intino/plugin/build/maven/PomCreator.java
+++ intino/src/io/intino/plugin/build/maven/PomCreator.java
@@ -46,7 +46,7 @@ public class PomCreator {
 	public PomCreator(Module module) {
 		this.module = module;
 		this.configuration = (LegioConfiguration) TaraUtil.configurationOf(module);
-		packageType = configuration.lifeCycle() == null || configuration.lifeCycle().package$() == null ? configuration.lifeCycle().package$().type() : null;
+		packageType = configuration.lifeCycle() == null || configuration.lifeCycle().package$() == null ? null: configuration.lifeCycle().package$().type();
 	}
 
 	public File frameworkPom() throws IOException {
