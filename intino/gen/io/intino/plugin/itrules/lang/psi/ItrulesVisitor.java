dda0877753f2ceff5edb6c95535ae5de6eeb3536
Octavio Roncal <octavioroncal@monentia.es>
2019-08-01 13:59:34 +0100

    updated version

---
+++ intino/gen/io/intino/plugin/itrules/lang/psi/ItrulesVisitor.java
@@ -0,0 +1,13 @@
+// This is a generated file. Not intended for manual editing.
+package io.intino.plugin.itrules.lang.psi;
+
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElementVisitor;
+
+public class ItrulesVisitor extends PsiElementVisitor {
+
+  public void visitPsiElement(@NotNull ItrulesPsiElement o) {
+    visitElement(o);
+  }
+
+}
