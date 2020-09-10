af0bfe36a2d5ba95dd3f120f7963cc51d1f0a4c2
Octavio Roncal <octavioroncal@monentia.es>
2020-09-10 13:03:17 +0100

    updated cesar integration

--- intino/src/io/intino/plugin/toolwindows/output/IntinoConsoleAction.java
+++ intino/src/io/intino/plugin/toolwindows/output/IntinoConsoleAction.java
@@ -4,7 +4,6 @@ import io.intino.cesar.box.schemas.ProcessInfo;
 import io.intino.cesar.box.schemas.ProcessStatus;
 
 public interface IntinoConsoleAction {
-
 	void onChanging();
 
 	void onProcessChange(ProcessInfo newProcess, ProcessStatus newProcessStatus);
