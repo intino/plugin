afeebdf4a7d935a3ff47ead5a64f717405570259
Octavio Roncal <octavioroncal@monentia.es>
2020-09-29 09:36:30 +0100

    retrocompatibility

--- intino/src/io/intino/plugin/codeinsight/livetemplates/TaraTemplateContext.java
+++ intino/src/io/intino/plugin/codeinsight/livetemplates/TaraTemplateContext.java
@@ -2,6 +2,7 @@ package io.intino.plugin.codeinsight.livetemplates;
 
 import com.intellij.codeInsight.template.TemplateActionContext;
 import com.intellij.codeInsight.template.TemplateContextType;
+import com.intellij.psi.PsiFile;
 import io.intino.plugin.lang.psi.TaraModel;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,5 +14,10 @@ public class TaraTemplateContext extends TemplateContextType {
 	@Override
 	public boolean isInContext(@NotNull TemplateActionContext context) {
 		return context.getFile() instanceof TaraModel;
+	}
+
+	@Override
+	public boolean isInContext(@NotNull PsiFile file, int offset) {
+		return file instanceof TaraModel;//COMPATIBILITY WITH 2020.1.4. TODO REMOVE METHOD
 	}
 }
