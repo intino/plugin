585050534a2929b401d277f357e984e513d53657
Octavio Roncal <octavioroncal@monentia.es>
2021-01-14 10:02:04 +0000

    fixed tara file document listener

--- intino/src/io/intino/plugin/codeinsight/languageinjection/TaraFileDocumentManagerListener.java
+++ intino/src/io/intino/plugin/codeinsight/languageinjection/TaraFileDocumentManagerListener.java
@@ -12,25 +12,6 @@ import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.NotNull;
 
 public class TaraFileDocumentManagerListener implements FileDocumentManagerListener {
-	@Override
-	public void beforeAllDocumentsSaving() {
-	}
-
-	@Override
-	public void beforeDocumentSaving(@NotNull Document document) {
-	}
-
-	@Override
-	public void beforeFileContentReload(VirtualFile file, @NotNull Document document) {
-	}
-
-	@Override
-	public void fileWithNoDocumentChanged(@NotNull VirtualFile file) {
-	}
-
-	@Override
-	public void fileContentReloaded(@NotNull VirtualFile file, @NotNull Document document) {
-	}
 
 	@Override
 	public void fileContentLoaded(@NotNull VirtualFile file, @NotNull Document document) {
@@ -42,9 +23,5 @@ public class TaraFileDocumentManagerListener implements FileDocumentManagerListe
 			if (psiFile != null && (psiFile.getName().startsWith("Java Fragment")))
 				ApplicationManager.getApplication().invokeLater(() -> new ReformatCodeProcessor(project, psiFile, null, false).run());
 		}
-	}
-
-	@Override
-	public void unsavedDocumentsDropped() {
 	}
 }
