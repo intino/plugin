4b32cbf6c23ee26413b34787c4ec24c881eaaf1b
Octavio Roncal <octavioroncal@monentia.es>
2022-05-25 12:14:15 +0100

    Updated intino Wizard Panel

--- intino/src/io/intino/plugin/project/module/IntinoWizardPanel.java
+++ intino/src/io/intino/plugin/project/module/IntinoWizardPanel.java
@@ -1,6 +1,15 @@
 package io.intino.plugin.project.module;
 
+import com.intellij.util.ui.UIUtil;
+import com.siyeh.ig.ui.UiUtils;
+import io.intino.alexandria.logger.Logger;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class IntinoWizardPanel {
 	private JRadioButton business;
@@ -10,7 +19,31 @@ public class IntinoWizardPanel {
 	private JRadioButton arquetype;
 	private JPanel root;
 	private JTextField groupId;
+	private JPanel diagramBox;
 
+
+	public IntinoWizardPanel() {
+	}
+
+	private void initImagePanel() {
+		final String name = UIManager.getLookAndFeel().getName();
+		boolean retina = UIUtil.isRetina();
+		final boolean dark = name.equalsIgnoreCase("Darcula") || name.equalsIgnoreCase("High Contrast");
+		try {
+			diagramBox = new JPanel();
+			final JLabel label = new JLabel(new ImageIcon(this.getClass().getResourceAsStream("/module/module_structure_" + (dark ? "dark" : "light") + (retina ? "_retina" : "") + ".png").readAllBytes()));
+			final Dimension dimension = new Dimension(400 * (retina ? 1 : 2), 400 * (retina ? 1 : 2));
+			diagramBox.setSize(dimension);
+			diagramBox.setPreferredSize(dimension);
+			diagramBox.setMaximumSize(dimension);
+			label.setSize(dimension);
+			label.setMaximumSize(dimension);
+			diagramBox.add(label);
+			diagramBox.setBounds(0, 50, dimension.width, dimension.height);
+		} catch (IOException e) {
+			Logger.error(e);
+		}
+	}
 
 	public JPanel panel() {
 		return root;
@@ -26,5 +59,9 @@ public class IntinoWizardPanel {
 
 	public String groupId() {
 		return groupId.getText();
+	}
+
+	private void createUIComponents() {
+		initImagePanel();
 	}
 }
