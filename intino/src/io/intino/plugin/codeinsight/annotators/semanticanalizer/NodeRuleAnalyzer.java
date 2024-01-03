175cf9e92d20a057eb667def4cf4f12e4602c1e6
Octavio Roncal <octavioroncal11@gmail.com>
2024-01-03 13:10:12 +0000

    fixed analyzer

--- intino/src/io/intino/plugin/codeinsight/annotators/semanticanalizer/NodeRuleAnalyzer.java
+++ intino/src/io/intino/plugin/codeinsight/annotators/semanticanalizer/NodeRuleAnalyzer.java
@@ -14,17 +14,17 @@ import io.intino.plugin.lang.psi.impl.IntinoUtil;
 import io.intino.plugin.lang.psi.impl.TaraPsiUtil;
 import io.intino.plugin.messages.MessageProvider;
 import io.intino.plugin.project.module.ModuleProvider;
-import io.intino.tara.dsls.MetaIdentifiers;
 import io.intino.tara.language.model.Mogram;
 import io.intino.tara.language.model.Tag;
 import io.intino.tara.language.model.rules.custom.Url;
 
 import static com.intellij.psi.search.GlobalSearchScope.moduleScope;
 import static io.intino.plugin.highlighting.TaraSyntaxHighlighter.UNRESOLVED_ACCESS;
+import static io.intino.tara.dsls.MetaIdentifiers.FACET;
+import static io.intino.tara.dsls.MetaIdentifiers.META_FACET;
 import static io.intino.tara.language.semantics.errorcollector.SemanticNotification.Level.ERROR;
 
 public class NodeRuleAnalyzer extends TaraAnalyzer {
-
 	private static final String RULES_PACKAGE = ".rules.";
 	private final String rulesPackage;
 	private final Rule rule;
@@ -38,22 +38,21 @@ public class NodeRuleAnalyzer extends TaraAnalyzer {
 
 	@Override
 	public void analyze() {
-		if (rule == null) {
-			error();
-			return;
-		} else if (node.is(Tag.Instance)) {
-			instanceError();
-			return;
-		}
-		final Module module = module();
-		if (rule.isLambda()) {
-			if (node.type().equalsIgnoreCase(MetaIdentifiers.FACET) || node.type().equalsIgnoreCase(MetaIdentifiers.META_FACET))
-				facetError();
-			return;
-		} else if (module == null) return;
+		if (rule == null) error();
+		else if (node.is(Tag.Instance)) instanceError();
+		else analyzeNodeRule();
+	}
 
-		PsiClass aClass = JavaPsiFacade.getInstance(rule.getProject()).findClass(rulesPackage + rule.getText(), moduleScope(module));
-		if (aClass == null && !isProvided()) error();
+	private void analyzeNodeRule() {
+		if (rule.isLambda()) {
+			if (node.type().equalsIgnoreCase(FACET) || node.type().equalsIgnoreCase(META_FACET)) facetError();
+		} else {
+			final Module module = module();
+			if (module != null) {
+				PsiClass aClass = JavaPsiFacade.getInstance(rule.getProject()).findClass(rulesPackage + rule.getText(), moduleScope(module));
+				if (aClass == null && !isProvided()) error();
+			}
+		}
 	}
 
 	private void facetError() {
